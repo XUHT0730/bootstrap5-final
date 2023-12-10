831ff4a32a0d78e0c41089be7e36d725f3ce5e65
XUHT0730 <roy1108730@gmail.com>
2023-12-10 16:17:30 +0800

    imput

---
+++ js/index.js
@@ -0,0 +1,54 @@
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+let subNav = document.getElementById('subNav'); 
+let sponsorBtn = document.getElementById('sponsorBtn');
+
+
+// form validation
+(function () {
+  "use strict";
+  var forms = document.querySelectorAll(".needs-validation");
+  Array.prototype.slice.call(forms).forEach(function (form) {
+  form.addEventListener(
+    "submit",
+    function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add("was-validated");
+    },
+    false
+    );
+  });
+})();
+
+function triggerSubNav(){ 
+  if(window.pageYOffset > 1900){
+    subNav.classList.remove('position-sticky','top-0','zIndexTop');
+		// 當 window.pageYOffset > 1900 條件成立時，
+    // 移除 subNav 的 'position-sticky','top-0','zIndexTop' 三個 class 
+  }else{
+    subNav.classList.add('position-sticky','top-0','zIndexTop');
+	 //  當 window.pageYOffset > 1900 條件不成立時，
+    // 把 subNav 加上 'position-sticky','top-0','zIndexTop' 三個 class 
+  }
+}
+window.addEventListener("scroll",triggerSubNav);
+
+function triggerSponsorBtn(){
+  if(window.pageYOffset > 400 && window.pageYOffset < 1800){
+    sponsorBtn.classList.remove('d-none');
+  }else{
+    sponsorBtn.classList.add('d-none');
+  }
+}
+window.addEventListener("scroll",triggerSponsorBtn);
+
+
